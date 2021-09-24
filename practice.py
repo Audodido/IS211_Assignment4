506ed96acec5b1830a5f0cc67223356a3cbde21c
connor <chanwick@gmail.com>
2021-09-24 16:01:32 -0400

    Up to part I.4.a

--- practice.py
+++ practice.py
@@ -1,1 +1,48 @@
-practice
+
+my_list = [3,5,6,8,11,12,14,15,17,18]
+
+#binary search
+
+# def half(alist, num):
+    
+
+#     if len(alist) == 0:
+#         return False
+    
+#     else:
+#         mid = len(alist) // 2
+        
+#     if alist[mid] == num:
+#         return True
+        
+#     else:
+#         print(alist[mid])
+#         if num < alist[mid]:
+#             return half(alist[:mid], num)
+#         else:
+#             return half(alist[mid + 1:], num)
+
+
+# print(half(my_list, 16))
+
+# hashing
+
+# print(130 % 13)
+
+# list = [113,117,97,100,114,108,116,105,99]
+# for l in list:
+#     print(l % 11)
+
+
+# #midsquare
+# x = 54 ** 2
+# print(x)
+# print(91 % 11)
+
+
+#bubble sort example:
+# bubble example
+# a = (19,1,9,7,3,10,13,15,8,12)
+# round 1 = (1, 9, 7, 3 10 13 15 8 12 19)
+# 1 7 3 9 10 13 8 12 15 19
+# 1 3 7 9 10 8 12 13 15 19
--- search_compare.py
+++ search_compare.py
@@ -1,21 +1,40 @@
 import argparse
 # other imports go here
-
+from timeit import Timer
+import timeit
+import time 
 import random
 
-def get_me_random_list(n):
-    """Generate list of n elements in random order
-    
-    :params: n: Number of elements in the list
-    :returns: A list with n elements in random order
-    """
-    a_list = list(range(n))
-    random.shuffle(a_list)
-    return a_list
-    
-def sequential_search(a_list,item):
-    pass
 
+
+def get_me_random_list(n):
+    """Generate list of n elements in random order"""
+
+    a_list = list(range(n)) 
+    random.shuffle(a_list)
+    # a_list = [random.randint(1, 9) for a in range(n)]
+    return a_list
+
+
+def sequential_search(a_list,item):
+
+    #start = time.time()
+
+    pos = 0
+    found = False
+
+    while pos < len(a_list) and not found:
+        if a_list[pos] == item:
+            found = True
+        else:
+            pos = pos+1
+
+    #end = time.time()
+
+    #elapsed = end - start 
+
+    return found #, elapsed
+    
 
 def ordered_sequential_search(a_list,item):
     pass
@@ -29,6 +48,18 @@ def binary_search_recursive(a_list,item):
     pass
 
 
+
 if __name__ == "__main__":
     """Main entry point"""
-    pass
+
+    t1 = Timer("sequential_search({},{})".format(get_me_random_list(500), -1), setup="from __main__ import sequential_search")
+    seq_ser_avg = t1.timeit(number=500)
+    #print(seq_ser_avg)
+   
+        ##https://stackabuse.com/python-string-interpolation-with-the-percent-operator/
+    print(f"Result is {sequential_search(get_me_random_list(500), -1)}. Operation required {seq_ser_avg} seconds")
+
+
+
+    # t1 = Timer("get_me_random_list({})".format(args.n), setup="from __main__ import get_me_random_list")
+    # print(f"random list generator takes: {t1.timeit(number=1000)} seconds")
